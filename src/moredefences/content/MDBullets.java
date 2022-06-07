ed46caed40dbeb24b7e09cf78940b68eafd3d039
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2022-06-07 22:55:10 +0200

    Update MDBullets.java

--- src/moredefences/content/MDBullets.java
+++ src/moredefences/content/MDBullets.java
@@ -21,9 +21,9 @@ public class MDBullets{
         }};
         multishotCoal = new BasicBulletType(3f, 6f){{ // speed, dmg
             ammoMultiplier = 1f;
-            incendAmount: 0.3f;
-            incendSpread: 0.5f;
-            incendChance: 0.9f;
+            incendAmount = 0.3f;
+            incendSpread = 0.5f;
+            incendChance = 0.9f;
         }};
         multishotSili = new BasicBulletType(3f, 8f){{ // speed, dmg
             ammoMultiplier = 2f;
