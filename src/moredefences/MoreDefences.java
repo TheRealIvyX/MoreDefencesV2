637e8753a91a111180b0bbb075174ef7749f8172
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2022-06-07 19:29:56 +0200

    Update MoreDefences.java

--- src/moredefences/MoreDefences.java
+++ src/moredefences/MoreDefences.java
@@ -21,7 +21,7 @@ public class MoreDefences extends Mod{
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.image(Core.atlas.find("more-defences-ivyx-multishot")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
