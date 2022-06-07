9bde89258b43335ce10c05e4fa227f9a5c88e753
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2022-06-07 22:53:26 +0200

    Create MDBlocks.java

---
+++ src/moredefences/content/MDBlocks.java
@@ -0,0 +1,61 @@
+package moredefences.content.blocks;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.DrawPart.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.ui.*;
+import mindustry.world.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.*;
+import static mindustry.type.ItemStack.*;
+
+public class MDBlocks{
+    public static Block
+
+    // turrets
+    multishot;
+
+    public static void load(){
+        multishot = new ItemTurret("multishot"){{
+            requirements(Category.turret, with(
+                Items.copper, 60,
+                Items.lead, 20
+            ));
+            ammo(
+                Items.lead, MDBullets.multishotLead,
+                Items.coal, MDBullets.multishotCoal,
+                Items.silicon, MDBullets.multishotSili
+            );
+            size = 1;
+            //hideDetails = false;
+            scaledHealth = 200;
+            reload = 60f;
+            inaccuracy = 3f;
+            range = 100f;
+            rotateSpeed = 5f;
+            shots = 8f;
+            spread = 1f;
+            range = 544f;
+            rotateSpeed = 2.5f;
+            //shootSound = Sounds.railgun;
+            //coolant = consumeCoolant(0.2f);
+        }};
+    }
+}
