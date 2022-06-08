d598c15b179a0a1e338799aa37aaed6d2c718e77
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2022-06-09 01:05:35 +0200

    Create MDItems.java

---
+++ src/moredefences/content/MDItems.java
@@ -0,0 +1,25 @@
+package moredefences.content.items;
+
+import arc.graphics.*;
+import mindustry.type.*;
+
+public class MDItems{
+    public static Item
+        aluminum, tin, cobalt;
+
+    public static void load(){
+        aluminum = new Item("aluminum", Color.valueOf("bdffdf")){{
+            //explosiveness = 0.2f;
+            //radioactivity = 0.2f;
+            //cost = 1.6f;
+            hardness = 2;
+        }};
+        tin = new Item("tin", Color.valueOf("bfab7d")){{
+            cost = 0.3f;
+            hardness = 1;
+        }};
+        cobalt = new Item("cobalt", Color.valueOf("a8b0e6")){{
+            hardness = 1;
+        }};
+    }
+}
