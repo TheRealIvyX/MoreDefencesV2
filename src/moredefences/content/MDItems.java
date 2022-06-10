ebb15ceafb98d0385a592e80b3634875f8369919
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2022-06-10 23:46:27 +0200

    hello nickel

--- src/moredefences/content/MDItems.java
+++ src/moredefences/content/MDItems.java
@@ -5,7 +5,7 @@ import mindustry.type.*;
 
 public class MDItems{
     public static Item
-        aluminum, tin, cobalt;
+        aluminum, tin, cobalt, nickel;
 
     public static void load(){
         aluminum = new Item("aluminum", Color.valueOf("bdffdf")){{
@@ -20,6 +20,9 @@ public class MDItems{
         }};
         cobalt = new Item("cobalt", Color.valueOf("a8b0e6")){{
             hardness = 1;
+        }};
+        nickel = new Item("nickel", Color.valueOf("E7DEA4")){{ // formerly zinc, which was formerly iron
+            hardness = 2;
         }};
     }
 }
