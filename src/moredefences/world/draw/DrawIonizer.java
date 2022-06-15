cd888cdbe7b5759220bf584a8a7870c7b9367d29
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2022-06-15 22:27:23 +0200

    plan b

---
+++ src/moredefences/world/draw/DrawIonizer.java
@@ -0,0 +1,50 @@
+package mindustry.world.draw;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.util.*;
+import mindustry.content.Items;
+import moredefences.content.items.MDItems; // thanks to Mnemotechnician#9967 for helping making this work
+import mindustry.entities.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.consumers.*;
+import mindustry.world.meta.*;
+
+public class DrawIonizer extends DrawBlock{
+    public TextureRegion graphiteRegion, beamRegion, radiumRegion, leadRegion, radiumPieceRegion;
+    @Override
+    public void draw(Building build){
+        Draw.alpha(build.items.get(Items.graphite) / build.itemCapacity);
+        Draw.rect(graphiteRegion, build.x, build.y);
+            
+        Draw.alpha(0);
+        if (build.items.get(Items.graphite) >= 1 && build.items.get(Items.lead) >= 2 && build.power.status == 1f) {Draw.alpha(1)};
+        Draw.rect(beamRegion, build.x, build.y);
+          
+        Draw.alpha(build.items.get(MDItems.radium) / build.itemCapacity);
+        Draw.rect(radiumRegion, build.x, build.y);
+            
+        Draw.alpha(build.items.get(Items.lead) / build.itemCapacity);
+        Draw.rect(leadRegion, build.x, build.y);
+          
+        Draw.alpha(build.items.get(MDItems.radium) / build.itemCapacity);
+        Draw.rect(radiumPieceRegion, build.x, build.y);
+      
+        Draw.alpha(1);
+    }
+    @Override
+    public void load(Block block){
+        graphiteRegion = Core.atlas.find(block.name + "-graphite");
+        beamRegion = Core.atlas.find(block.name + "-beams");
+        radiumRegion = Core.atlas.find(block.name + "-radium");
+        leadRegion = Core.atlas.find(block.name + "-lead");
+        radiumPieceRegion = Core.atlas.find(block.name + "-radium-piece");
+    }
+}
