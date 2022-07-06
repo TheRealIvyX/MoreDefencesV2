4e6a878b2fb21f0f354610ee9407fd5c70c57ca7
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2022-07-06 14:15:59 +0200

    when the imports part 2.1 space colony ark

--- src/moredefences/entities/bullet/FreezeBulletType.java
+++ src/moredefences/entities/bullet/FreezeBulletType.java
@@ -3,15 +3,22 @@ package moredefences.entities.bullet;
 import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
-import betamindy.content.*;
-import betamindy.graphics.*;
+import arc.util.*;
+import arc.util.io.*;
 import mindustry.content.*;
 import mindustry.entities.*;
 import mindustry.entities.bullet.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
+import mindustry.world.*;
 
 import moredefences.content.blocks.*;
+
+import static arc.graphics.g2d.Draw.rect;
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
+import static mindustry.Vars.*;
 
 public class FreezeBulletType extends BasicBulletType {
     public float freezeChance = 0.2f;
@@ -31,7 +38,7 @@ public class FreezeBulletType extends BasicBulletType {
                 if (other.team != b.owner.team) {
                     Rand().setSeed(b.id+other.id+b.owner.id+other.x+other.y+b.x+b.y+b.owner.x+b.owner.y);
                     Tile t = Vars.world.tile(Mathf.round(other.x / 8), Mathf.round(other.y / 8));
-                    Block o = t.block();
+                    Building o = t.block();
                     if ((o.size >= 1 && o.size <= Math.min(2, Math.max(1, maxTargetSize))) && Rand().random(0,1) <= freezeChance) {
                         t.setAir();
                         if (o.size == 1) {
