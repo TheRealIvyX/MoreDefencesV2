d983c39db502c3ed1a100ebba7083f27e2b7c07a
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2022-07-06 14:20:25 +0200

    Update FreezeBulletType.java

--- src/moredefences/entities/bullet/FreezeBulletType.java
+++ src/moredefences/entities/bullet/FreezeBulletType.java
@@ -36,9 +36,9 @@ public class FreezeBulletType extends BasicBulletType {
         if (freezeRadius > 0) {
             Units.nearbyBuildings(b.x, b.y, freezeRadius, cons(other -> {
                 if (other.team != b.owner.team) {
-                    Rand().setSeed(b.id+other.id+b.owner.id+other.x+other.y+b.x+b.y+b.owner.x+b.owner.y);
+                    Rand().setSeed(b.id+other.id+other.x+other.y+b.x+b.y);
                     Tile t = Vars.world.tile(Mathf.round(other.x / 8), Mathf.round(other.y / 8));
-                    Building o = t.block();
+                    Block o = t.block();
                     if ((o.size >= 1 && o.size <= Math.min(2, Math.max(1, maxTargetSize))) && Rand().random(0,1) <= freezeChance) {
                         t.setAir();
                         if (o.size == 1) {
